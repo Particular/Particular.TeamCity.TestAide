cb46c72b88d734b27f2b92e9f977a88f77a08aa7
Brandon Ording <bording@gmail.com>
2021-01-28 16:24:48 -0500

    Fix rule violations

--- src/Particular.TeamCity.TestAide/Program.cs
+++ src/Particular.TeamCity.TestAide/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-
-namespace Particular.TeamCity.TestAide
+﻿namespace Particular.TeamCity.TestAide
 {
+    using System;
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
